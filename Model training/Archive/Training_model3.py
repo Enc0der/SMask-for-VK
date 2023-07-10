1914ae0d97222aeb530b230ec118646f10a65378
Aleksandra <70717995+Enc0der@users.noreply.github.com>
2023-07-10 12:43:11 +0500

    Update Training_model3.py

--- Model training/Archive/Training_model3.py
+++ Model training/Archive/Training_model3.py
@@ -1,6 +1,5 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-from config import db_password
 import pandas as pd
 # Import dependencies
 from sklearn.model_selection import train_test_split
@@ -9,50 +8,58 @@ from sklearn.preprocessing import LabelEncoder
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Activation
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 from keras.layers.advanced_activations import LeakyReLU
 
-import matplotlib.pyplot as plt
 import numpy as np
-
-Using TensorFlow backend.
+import json
 
 
 # read data from Postgres Database
-engine = create_engine('postgresql://postgres:'+ db_password +'@localhost:5432/InstroPitch_DB')
+with open('settings.json') as settings_file:
+    config = json.load(settings_file)
+    print(config)
 
-notes_df = pd.read_sql_table('Notes_Spectrogram_Table',engine)
+url_object = sqlalchemy.engine.url.URL.create(
+
+    username=config['db_connection']['username'],
+    password=config['db_connection']['password'],
+    host='pgsql',
+    database=config['db_connection']['database'],
+    drivername=config['db_connection']['driver'],
+)
+# read data from Postgres Database
+engine = create_engine(url_object)
+
+notes_df = pd.read_sql_table('Notes_Spectrogram_Table', engine)
 # notes_df
 
 # Convert Spectrograms from list to ndarray
 notes_df['Spectrogram'] = notes_df['Spectrogram'].apply(lambda x: np.array(x))
 
 type(notes_df['Spectrogram'].iloc[0])
-numpy.ndarray
-MODEL - Neural Network
+
 
 # Create train and test data sets
 X_series = notes_df["Spectrogram"]
 y = notes_df["Note"]
 
 # Parameters
-channels = 1 # number of audio channels
+channels = 1  # number of audio channels
 spectrogram_shape = X_series[1].shape + (channels,)
 batch = spectrogram_shape[1]
-[7]
-X_series.shape
-(2913,)
+# X_series.shape
+# (2913,)
 
 count = 0
 
 for i in X_series[1]:
-    count =+ 1
-
+    count += 1
 
 
 # Reshape X into size of spectrogram and convert to ndarray
-X = np.array([i.reshape( (spectrogram_shape) ) for i in X_series])
+X = np.array([i.reshape(spectrogram_shape) for i in X_series])
 
 # Encode pitches
 
@@ -71,10 +78,10 @@ y_test_hot = to_categorical(y_test)
 y_train_hot = to_categorical(y_train)
 
 # Troubleshooting queries
-type(X_train[1])
-X_train[1].shape
+# type(X_train[1])
+# X_train[1].shape
 # X_train[1]
-(22, 128, 1)
+# (22, 128, 1)
 
 # # Model
 # model = Sequential()
@@ -122,23 +129,28 @@ X_train[1].shape
 
 # Model
 notes_model = Sequential()
-notes_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(spectrogram_shape),padding='same'))
+notes_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=spectrogram_shape, padding='same'))
 notes_model.add(LeakyReLU(alpha=0.1))
-notes_model.add(MaxPooling2D((2, 2),padding='same'))
-notes_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+notes_model.add(MaxPooling2D((2, 2), padding='same'))
+
+
+notes_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
 notes_model.add(LeakyReLU(alpha=0.1))
-notes_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
-notes_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+notes_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+
+notes_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
 notes_model.add(LeakyReLU(alpha=0.1))
-notes_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+notes_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+
 notes_model.add(Flatten())
 notes_model.add(Dense(128, activation='linear'))
 notes_model.add(LeakyReLU(alpha=0.1))
 notes_model.add(Dense(12, activation='softmax'))
 
-notes_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+notes_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 
 # notes_model.summary()
 
-notes_train = notes_model.fit(X_train, y_train_hot, batch_size=batch,epochs=8,verbose=1,validation_data=(X_test, y_test_hot))
-
+notes_train = notes_model.fit(X_train, y_train_hot, batch_size=batch, epochs=8, verbose=1, validation_data=(X_test, y_test_hot))
