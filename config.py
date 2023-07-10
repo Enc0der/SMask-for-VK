a51497595ecc68feb955c9e385cd618e7df83685
Aleksandra <70717995+Enc0der@users.noreply.github.com>
2023-07-10 12:37:43 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-token = ''
-aws_secret_access_key='guUYUDdCAgAJp757thbFLaqd2Y9H7XefW8P6FkbwLcFM'
+TOKEN = '5919770894:AAFfq2Hd2oC9I3jKTAUXYvs91K4D0Y-Q9fU'
+
+aws_secret_access_key = 'guUYUDdCAgAJp757thbFLaqd2Y9H7XefW8P6FkbwLcFM'
